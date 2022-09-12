a5e149129dbdbf645a511a7ca3d9ba8e6125d88b
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-09-12 17:30:55 -0300

    feat: finalizando as configurações inciiais

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 
-app.get("/ads", (request, response) => {
+app.get("/ads", (request: Request, response: Response) => {
   return response.json([
     { id: 1, name: "Anuncio 1" },
     { id: 2, name: "Anuncio 2" },
