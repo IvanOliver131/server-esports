3a581579539282b0f8fe22460c4120d7d791a4a1
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-09-14 17:10:04 -0300

    feat: adição de novas rotas

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,14 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+app.get("/games", (request: Request, response: Response) => {
+  return response.json([]);
+});
+
+app.post("/ads", (request: Request, response: Response) => {
+  return response.status(201).json([]);
+});
+
 app.get("/ads", (request: Request, response: Response) => {
   return response.json([
     { id: 1, name: "Anuncio 1" },
